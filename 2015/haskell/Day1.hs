da84eefbcfee541ffeddef4671b6124c5e69b6f1
Emilia P. Stoyanova <emiliaps31@gmail.com>
2024-07-03 16:10:23 +0200

    Simplify 2015/Day(1,2).hs implementations
    * Remove old commented out implementations for Day1 and use Integer
    * replace folds in Day2 with sum/product

--- 2015/haskell/Day1.hs
+++ 2015/haskell/Day1.hs
@@ -3,28 +3,21 @@ module Day1 where
 import Data.Maybe
 import Data.Tuple.Extra
 import Data.Text (Text)
-import qualified Data.Text as T
+import qualified Data.Text    as T
 import qualified Data.Text.IO as T
-import qualified TextShow as T
+import qualified TextShow     as T
 
 main :: IO ()
 main = T.interact (T.showt . solve)
 
-solve :: Text -> (Int, Int)
+solve :: Text -> (Integer, Integer)
 solve = p1 &&& p2 . T.unpack
 
-p1 :: Text -> Int
+p1 :: Text -> Integer
 p1 = T.foldr selector 0
--- p1 = uncurry (-) . both T.length . T.partition ('('==). T.dropWhileEnd isSpace
 
-p2 :: String -> Int
+p2 :: String -> Integer
 p2 = length . takeWhile (>=0) . scanl (flip selector) 0
--- p2 :: String -> Int
--- p2 = fromLeft maxBound . bimapBoth (snd . fst) . mapAccumM goM (0,0)
---   goM :: (Int, Int) -> Char -> Either ((Int, Int), Char) ((Int, Int), Char)
---   goM (acc, iter) elem@'(' = Right ((succ acc, succ iter), elem)
---   goM (x@0, iter) elem@')' = Left  ((pred   x, succ iter), elem)
---   goM (acc, iter) elem@')' = Right ((pred acc, succ iter), elem)
 
-selector :: Enum a => Char -> a -> a
+selector :: Char -> Integer -> Integer
 selector = fromMaybe id . flip lookup [('(', succ), (')', pred)]
--- 2015/haskell/Day2.hs
+++ 2015/haskell/Day2.hs
@@ -1,37 +1,36 @@
-{-# Language OverloadedStrings #-}
-
+{-# LANGUAGE OverloadedStrings #-}
 module Day2 where
 
 import Data.Either
 import Data.List
 import Data.Tuple.Extra
 import Data.Text (Text)
-import qualified Data.Text as T
-import qualified Data.Text.IO as T
+import qualified Data.Text      as T
+import qualified Data.Text.IO   as T
 import qualified Data.Text.Read as T
-import qualified TextShow as T
+import qualified TextShow       as T
 
 type Solver = [[Integer]] -> Integer
-type Helper = [Integer] -> Integer
+type Helper =  [Integer]  -> Integer
 
 main :: IO ()
 main = T.interact (T.showt . solve . map parse . T.lines)
 
-solve :: [[Integer]] -> (Integer, Integer)
-solve = p1 &&& p2
-
 parse :: Text -> [Integer]
 parse = rights . map (fmap fst . T.decimal) . T.splitOn "x"
+
+solve :: [[Integer]] -> (Integer, Integer)
+solve = p1 &&& p2
 
 p1, p2 :: Solver
 p1 = common area smallestSide
 p2 = common ribbon bow
 
 common :: Helper -> Helper -> Solver
-common f g = foldr1 (+) . map (liftA2 (+) f g)
+common f g = sum . map (liftA2 (+) f g)
 
 area, smallestSide, ribbon, bow :: Helper
 area [l,w,h] = 2*l*w + 2*w*h + 2*h*l
-smallestSide = foldr1 (*)                  . tail . sortBy (flip compare)
-ribbon       = foldr1 (+) . take 4 . cycle . tail . sortBy (flip compare)
-bow          = foldr1 (*)
+smallestSide = product              . take 2 . sort
+ribbon       = sum . take 4 . cycle . take 2 . sort
+bow          = product
