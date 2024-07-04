a5839c577a3b6a399d474814545b44915a140a52
Emilia P. Stoyanova <emiliaps31@gmail.com>
2024-07-04 07:54:11 +0200

    Add 2015/Day4.hs

---
+++ 2015/haskell/Day4.hs
@@ -0,0 +1,34 @@
+module Day4 where
+
+import Crypto.Hash
+import Data.ByteString (ByteString)
+import Data.Composition
+import Data.List
+import Data.String
+import Data.Tuple.Extra
+
+-- Make sure there's no trailing newline character at the end
+main :: IO ()
+main = interact (show . solve . parse)
+
+parse :: String -> [(Integer, String)]
+parse key = map (toSnd $ md5 . (++) key . show) [1..]
+
+solve :: [(Integer, String)] -> (Integer, Integer)
+solve = p1 &&& p2
+
+p1, p2 :: [(Integer, String)] -> Integer
+p1 = common "00000"
+p2 = common "000000"
+
+common :: String -> [(Integer, String)] -> Integer
+common pre = fst . findFst (isPrefixOf pre . snd)
+
+findFst :: (a -> Bool) -> [a] -> a
+findFst = head .: filter
+
+md5 :: String -> String
+md5 = show . (hash :: ByteString -> Digest MD5) . fromString
+
+toSnd :: (a -> b) -> a -> (a, b)
+toSnd = (&&&) id
