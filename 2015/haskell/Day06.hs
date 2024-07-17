069c42cda38a75e045dc6de286ee6b5039ab5eac
Emilia P. Stoyanova <emiliaps31@gmail.com>
2024-07-17 13:13:30 +0200

    Add grid counting logic to 2015/Day06.hs

--- 2015/haskell/Day06.hs
+++ 2015/haskell/Day06.hs
@@ -1,12 +1,15 @@
 module Day06 where
 
 import Control.Applicative
+import Control.Monad
 import Data.Char
-import Data.Maybe
 import Data.Composition
+import Data.Function
+import Data.Maybe
+import Data.Tuple.Extra
 import Text.ParserCombinators.ReadP
 
-newtype Coords = Coords (Int, Int) deriving (Show, Eq)
+newtype Coords = Coords { unCoords :: (Int, Int) } deriving (Show, Eq)
 data Dir = On     Coords Coords
          | Off    Coords Coords
          | Toggle Coords Coords deriving (Show, Eq)
@@ -14,6 +17,22 @@ data Dir = On     Coords Coords
 parse :: String -> [Dir]
 parse = concat . map fst . (readP_to_S $ many1 $ dir <* skipSpaces)
 
+--- Grid logic utilities ---
+cntCells :: Coords -> Coords -> Int
+cntCells = uncurry (on (*) (uncurry range))
+         . zipPair .: curry (both unCoords)
+
+range :: Int -> Int -> Int
+range = succ . abs .: (-)
+
+-- zipPair :: ((a, b), (c, d)) -> ((a, c), (b, d))
+zipPair = on (&&&) (join (***)) fst snd
+-- zipPair = join (***) fst &&& join (***) snd
+-- zipPair = (fst *** fst) &&& (snd *** snd)
+-- zipPair ((x1, y1), (x2, y2)) = ((x1, x2), (y1, y2))
+-----------------------
+
+--- ReadP parsing utilities ---
 dir :: ReadP Dir
 dir = makeDir <$> command <* skipSpaces <*> coords <* skipSpaces <* string "through" <* skipSpaces <*> coords
 
@@ -33,3 +52,4 @@ int = read <$> munch1 isDigit
 
 surround :: ReadP a -> ReadP b -> ReadP (a, a)
 surround p sep = (,) <$> p <* sep <*> p
+-------------------------------
