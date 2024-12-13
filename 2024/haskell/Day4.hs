3e6641cc85a7f357806afa2cef44b7c8f8bf8d32
Emilia P. Stoyanova <emiliaps31@gmail.com>
2024-12-13 15:10:07 +0100

    Better 2024/**/Day4.hs solution

--- 2024/haskell/Day4.hs
+++ 2024/haskell/Day4.hs
@@ -11,31 +11,24 @@ parse :: String -> [String]
 parse = lines
 
 solve :: [String] -> (Int, Int)
-solve = p1  &&& p2
+solve = p1 &&& p2
 
 p1, p2 :: [String] -> Int
-p1 = sum . map xmasCount . concat . transforms
+p1 = length . concatMap (filter ("XMAS"==) . windows 4). concat . transforms
 p2 = foldr masCount 0 . submatrices 3
 
 -------------
 --- Utils ---
 -------------
 transforms :: [String] -> [[String]]
-transforms = sequence [ id                                    -- rows
-                      , map reverse                           -- rows     rev
-                      , transpose                             -- cols
-                      , map reverse . transpose               -- cols     rev
-                      , diagonals                             -- off diag
-                      , map reverse . diagonals               -- off diag rev
-                      , diagonals . map reverse               -- diag
-                      , map reverse . diagonals . map reverse -- diag     rev
-                      ]
-
-xmasCount :: String -> Int
-xmasCount = flip go 0 where
-  go []                    n =             n
-  go ('X':'M':'A':'S':xs)  n = go xs (succ n)
-  go (_:xs)                n = go xs       n
+transforms = sequence [ id                                      -- rows
+                      , map reverse                             -- rows    rev
+                      , transpose                               -- cols
+                      , map reverse . transpose                 -- cols    rev
+                      , diagonals                               -- off-dia
+                      , map reverse . diagonals                 -- off-dia rev
+                      , diagonals . map reverse                 --     dia
+                      , map reverse . diagonals . map reverse ] --     dia rev
 
 masCount :: [String] -> Int -> Int
 masCount [['M',_,'M'],[_,'A',_],['S',_,'S']] n = succ n
@@ -46,8 +39,8 @@ masCount _                                   n =      n
 
 submatrices :: Int -> [[a]] -> [[[a]]]
 submatrices k matrix = [ map (take k . drop j) (take k (drop i matrix))
-                      | i <- [0 .. n - k], j <- [0 .. n - k]
-                      ] where n = length matrix
+                       | i <- [0 .. n - k], j <- [0 .. n - k]
+                       ] where n = length matrix
 
--- windows :: Int -> [a] -> [[a]]
--- windows n = dropEnd (pred n) . transpose . take n . tails
+windows :: Int -> [a] -> [[a]]
+windows n = dropEnd (pred n) . transpose . take n . tails
