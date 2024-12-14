375ec17502443024a1f8661c06c3225660066e9f
Emilia P. Stoyanova <emiliaps31@gmail.com>
2024-12-14 15:08:44 +0100

    Prase the input for 2024/**/Day6.hs

---
+++ 2024/haskell/Day6.hs
@@ -0,0 +1,29 @@
+module Day6 where
+
+import Data.List.Extra
+import Data.Tuple.Extra
+import Data.Maybe
+
+type Coords = (Int, Int) -- (x, y)
+
+main :: IO ()
+main = interact (show . solve . parse)
+
+parse :: a -> a
+parse = undefined
+
+solve :: a -> (a, a)
+solve = p1 &&& p2
+
+p1, p2 :: a -> a
+p1 = undefined
+p2 = undefined
+
+-------------
+--- Utils ---
+-------------
+parseCoords :: [String] -> [Coords]
+parseCoords = concat . zipWithFrom (map . flip (,)) 0 . map (elemIndices '#')
+
+parseGuard :: [String] -> Coords
+parseGuard = head . catMaybes . zipWith (liftA2 $ flip (,)) (map Just [0..]) . map (elemIndex '^')
